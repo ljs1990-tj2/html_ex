a0ae19faee1bc15f8125ed11440301d7bdd3a9bc
JS Lee <ljs1990.tj2@gmail.com>
2024-07-30 18:11:15 +0900

    2024.07.30 commit

--- 20240729/server.js
+++ 20240729/server.js
@@ -36,6 +36,46 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/qwer', async (req, res) => {
+  var { stuNo, stuGrade } = req.query;
+  var query = `UPDATE STUDENT SET STU_GRADE = ${stuGrade} WHERE STU_NO = ${stuNo}`;
+  await connection.execute(query, [], { autoCommit: true });
+
+  // res.send('Hello World');
+  // UPDATE STUDENT SET STU_GRADE = 내가보낸학년 WHERE STU_NO = 내가보낸학번
+});
+
+// 학번으로 삭제
+app.get('/stu-delete', async (req, res) => {
+  var { stuNo } = req.query;
+  var query = `DELETE FROM STUDENT WHERE STU_NO = ${stuNo}`;
+  await connection.execute(query, [], { autoCommit: true });
+
+  res.json({message : "잘 삭제되었다!"});
+});
+
+// 중복체크
+app.get('/idCheck', async (req, res) => {
+  var { stuNo } = req.query;
+  var query = `SELECT COUNT(*) AS CNT FROM STUDENT WHERE STU_NO = ${stuNo}`;
+  const result = await connection.execute(query);
+
+  const columnNames = result.metaData.map(column => column.name);
+  // 쿼리 결과를 JSON 형태로 변환
+  const rows = result.rows.map(row => {
+    // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+    const obj = {};
+    columnNames.forEach((columnName, index) => {
+      obj[columnName] = row[index];
+    });
+    return obj;
+  });
+
+  res.json(rows);
+});
+
+
+
 app.get('/list', async (req, res) => {
   const { keyword, grade } = req.query;
   try {
@@ -71,6 +111,28 @@ app.get('/delete', async (req, res) => {
     res.status(500).send('Error executing query');
   }
 });
+
+app.get('/update', async (req, res) => {
+  const { stuNo, stuName, stuDept, stuGrade, stuGender } = req.query;
+  var query = `UPDATE 
+               STUDENT SET 
+                STU_NAME = '${stuName}',
+                STU_DEPT = '${stuDept}',
+                STU_GRADE = '${stuGrade}',
+                STU_GENDER = '${stuGender}'
+              WHERE STU_NO = '${stuNo}'`
+  console.log(query);
+  try {
+    await connection.execute(query, [], { autoCommit: true });
+   
+    res.json([{message : "수정 되었습니다"}]);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+
 
 // 서버 시작
 app.listen(3000, () => {
